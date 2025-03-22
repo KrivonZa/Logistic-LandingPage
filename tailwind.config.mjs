477864541a35f82657e90677164375cee81eee1e
Kevin Truong <kevintruong6587@gmail.com>
2025-03-22 17:19:54 +0700

    feat: Update color for landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        primary: '#4348BD',
-        secondary: '#FF712C'
-      }
-    }
+        primary: "#4348BD",
+        secondary: "#FF914D",
+      },
+    },
   },
-  plugins: []
-}
-
-//màu chính: #4348BD
+  plugins: [],
+};
