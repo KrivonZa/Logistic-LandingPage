1bec129814f7f7791ca0d496dc8105d211f4b897
Kevin Truong <kevintruong6587@gmail.com>
2025-07-02 20:37:15 +0700

    update: refactor UI

--- src/components/index/index.ts
+++ src/components/index/index.ts
@@ -2,4 +2,3 @@ export { default as Reasons } from "./Reasons.astro";
 export { default as Stats } from "./Stats.astro";
 export { default as Testimonials } from "./Testimonials.astro";
 export { default as CTA } from "./CTA.astro";
-export { default as Partners } from "./Partners.astro";
